493263a2ec6715ced8cc5b95ac14b0bdb83cb17d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-02-20 15:13:14 +0000

    Commit

--- vagrant.d/puppet/manifests/default.pp
+++ vagrant.d/puppet/manifests/default.pp
@@ -1,98 +1,12 @@
 #######################################################################################################################
 # Main Manifest
 #######################################################################################################################
-
 $vsConfig       = parseyaml( $facts['vs_config'] )
 
 node default
 {
-    include epel
-	include stdlib
+	include devops
 	
-	$vsConfig['packages'].each |Integer $index, String $value|
-	{
-        if ( $value == 'git' ) {
-            require git
-                
-            git::config { 'user.name':
-                value => $vsConfig['git']['userName'],
-                user    => 'vagrant',
-            }
-            git::config { 'user.email':
-                value => $vsConfig['git']['userEmail'],
-                user    => 'vagrant',
-            }
-        } elsif ( $value == 'gitflow' and $operatingsystem == 'Ubuntu' ) {
-            package { 'git-flow':
-                ensure => present,
-            }
-        } else {
-            package { $value:
-                ensure => present,
-            }
-        }
-    }
-	
-	#notice( "SERVICE GITLAB VALUE: ${$vsConfig['services']['gitlab']}" )
-	
-	if ( $vsConfig['services']['jenkins'] == true )
-    {
-        class{ 'jenkins':
-            config_hash => {
-                'JENKINS_PORT'	=> { 'value' => '8080' },
-            }
-        }
-        
-        jenkins::plugin { 'git':
-			version => '1.1.11',
-		}
-		
-		jenkins::plugin { 'ftppublisher':
-		
-		}
-		
-		jenkins::plugin { 'publish-over':
-        
-        }
-		jenkins::plugin { 'publish-over-ftp':
-		
-		}
-		
-		jenkins::plugin { 'jobConfigHistory':
-		
-		}
-		
-		jenkins::plugin { 'phing':
-		
-		}
-		
-		# Install Maven
-		class { "maven::maven":
-		    #version => "3.2.5", # version to install
-		    # you can get Maven tarball from a Maven repository instead than from Apache servers, optionally with a user/password
-		    repo => {
-				#url => "http://repo.maven.apache.org/maven2",
-				#username => "",
-				#password => "",
-		    }
-		}
-		
-		
-    }
-    
-	if ( $vsConfig['services']['gitlab'] == true )
-    {
-    	class { 'gitlab':
-            external_url => 'http://devops.lh',
-            unicorn => {
-                'worker_timeout'	=> 300,
-                'port'				=> 8081
-            },
-        }
-    }
-    
-    
-
 	# puppet module install saz-sudo --version 5.0.0
 	sudo::conf { "vagrant":
 	    ensure			=> "present",
