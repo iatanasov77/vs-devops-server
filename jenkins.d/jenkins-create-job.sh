0d4c8683785dda8bf620b19cd80c34c5be4be1db
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-07-14 15:39:05 +0000

    Litle refactoring

--- jenkins.d/jenkins-create-job.sh
+++ jenkins.d/jenkins-create-job.sh
@@ -4,30 +4,4 @@
 # Create Jenkins create job
 # 
 
-cat <<EOF | java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ -http -auth admin:admin create-job VankoSoft.Org
-<?xml version='1.1' encoding='UTF-8'?>
-<flow-definition plugin="workflow-job@2.39">
-  <actions>
-    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin="pipeline-model-definition@1.7.1"/>
-    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin="pipeline-model-definition@1.7.1">
-      <jobProperties/>
-      <triggers/>
-      <parameters/>
-      <options/>
-    </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>
-  </actions>
-  <description></description>
-  <keepDependencies>false</keepDependencies>
-  <properties>
-    <org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
-      <triggers/>
-    </org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
-  </properties>
-  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.81">
-    <script></script>
-    <sandbox>true</sandbox>
-  </definition>
-  <triggers/>
-  <disabled>false</disabled>
-</flow-definition>
-EOF
+source /vagrant/jenkins.d/jobs/the_first.sh
