1e6a391e831f74bd438a2264d0c6bcd7aeeb83c6
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-07-14 14:59:53 +0000

    Commit

--- public/index.php
+++ public/index.php
@@ -1,1 +1,27 @@
-<h1>NO DASHBOARD</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta charset="utf-8" />
+    </head>
+    
+    <body>
+    	<div id="container">
+        	<div style="border: 1px solid black; width: 300px; height: 150px; margin: 20px; text-align: center; float: left;">
+        		<h3>Jenkins Master Automation Server</h3>
+        		<a href="http://devops.lh:8080" target="__blank">Open Jenkins</a>
+        		<p>
+            		<div>User: admin</div>
+                	<div>Pass: admin</div>
+                </p>
+        	</div>
+        	<div style="border: 1px solid black; width: 300px; height: 150px; margin: 20px; text-align: center; float: left;">
+        		<h3>Icinga2 Monitoring Server</h3>
+        		<a href="http://devops.lh/icingaweb2/" target="__blank">Open Icinga2</a>
+        		<p>
+            		<div>User: icingaadmin</div>
+                	<div>Pass: icinga</div>
+                </p>
+        	</div>
+        </div>
+    </body>
+</html>
