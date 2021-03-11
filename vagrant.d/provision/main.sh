18dbd2c7571c6a0548b866b1a9b2a535c24b4b83
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-03-11 15:29:36 +0000

    Commit

--- vagrant.d/provision/main.sh
+++ vagrant.d/provision/main.sh
@@ -5,6 +5,6 @@ source /vagrant/vagrant.d/provision/make_swap.sh
 #source /vagrant/vagrant.d/provision/install_php.sh
 source /vagrant/vagrant.d/provision/install_puppet.sh
     
-if [ $ID == "centos" ] && [ $VERSION_ID == "8" ]; then
-    #source /vagrant/vagrant.d/provision/centos8_install_gitlab.sh
-fi
+#if [ $ID == "centos" ] && [ $VERSION_ID == "8" ]; then
+#    source /vagrant/vagrant.d/provision/centos8_install_gitlab.sh
+#fi
