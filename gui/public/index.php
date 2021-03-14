9beda7fc438ad1a0d349c9c43a0050ac98d5baf4
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-03-14 19:10:30 +0000

    Ading many subsystems.

--- gui/public/index.php
+++ gui/public/index.php
@@ -1,3 +1,6 @@
+<?php 
+    $configSubsystems = json_decode( file_get_contents( dirname( __FILE__ ) . "/../var/subsystems.json" ), true );
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -20,7 +23,27 @@
         	</div>
         </div>
         <div style="clear: both;"></div>
+        <div id="container">
+        <?php if ( $configSubsystems['terraform']['enabled'] ) {?>
+        	<div style="border: 1px solid black; width: 300px; height: 150px; margin: 20px; text-align: center; float: left;">
+        		<h3>Terraform</h3>
+        		<p>
+            		<div>cd /vagrant/terraform.d</div>
+					<div>terraform init</div>
+					<div>terraform apply</div>
+				</p>
+				<p>
+					<div>And then open in your host browser http://docker.lh</div>
+                </p>
+        	</div>
+        <?php } ?>
+        </div>
+        <div style="clear: both;"></div>
+        
+
+
     	<div id="container">
+    	<?php if ( $configSubsystems['jenkins']['enabled'] ) {?>
         	<div style="border: 1px solid black; width: 300px; height: 150px; margin: 20px; text-align: center; float: left;">
         		<h3>Jenkins Master Automation Server</h3>
         		<a href="http://devops.lh:8080" target="__blank">Open Jenkins</a>
@@ -29,7 +52,22 @@
                 	<div>Pass: admin</div>
                 </p>
         	</div>
-        	<div style="border: 1px solid black; width: 300px; height: 150px; margin: 20px; text-align: center; float: left;">
+        <?php } ?>
+        </div>
+        <div style="clear: both;"></div>
+    	<div id="container">
+    	<?php if ( $configSubsystems['nagios']['enabled'] ) {?>
+    		<div style="border: 1px solid black; width: 300px; height: 150px; margin: 20px; text-align: center; float: left;">
+        		<h3>Nagios Monitoring Server</h3>
+        		<a href="http://devops.lh/nagios/" target="__blank">Open Nagios</a>
+        		<p>
+            		<div>User: nagiosadmin</div>
+                	<div>Pass: nagiosadmin</div>
+                </p>
+        	</div>
+        <?php } ?>	
+        <?php if ( $configSubsystems['icinga']['enabled'] ) {?>
+    		<div style="border: 1px solid black; width: 300px; height: 150px; margin: 20px; text-align: center; float: left;">
         		<h3>Icinga2 Monitoring Server</h3>
         		<a href="http://devops.lh/icingaweb2/" target="__blank">Open Icinga2</a>
         		<p>
@@ -37,6 +75,7 @@
                 	<div>Pass: icinga</div>
                 </p>
         	</div>
+        <?php } ?>
         </div>
     </body>
 </html>
