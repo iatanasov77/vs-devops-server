386e362c94c9cf4de45a8c5521a99d46323e5599
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-06-03 13:04:34 +0000

    Fix Jenkins Installation and Configuration.

--- vagrant.d/provision/main.sh
+++ vagrant.d/provision/main.sh
@@ -2,6 +2,16 @@
 
 source /vagrant/vagrant.d/provision/detect_linux.sh
 source /vagrant/vagrant.d/provision/make_swap.sh
+
+#######################################################################################
+# The CentOS Linux 8 packages have been removed from the mirrors. 
+# If you haven’t already, convert any CentOS Linux 8 installations to Stream 8
+#######################################################################################
+if [[ $ID == "centos" && $VERSION_ID == "8" ]]; then
+    dnf -y --disablerepo '*' --enablerepo extras swap centos-linux-repos centos-stream-repos
+    dnf -y distro-sync
+fi
+
 #source /vagrant/vagrant.d/provision/install_php.sh
 source /vagrant/vagrant.d/provision/install_puppet.sh
     
